e4b07fa847bce7d58f41c3cf91c40b64064522ab
Javier Ubeda <javier.ubeda@ogilvy.com>
2023-07-17 17:23:25 +0200

    Adding Tag Select and Country Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,59 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
 import Recipes from './components/Recipes.jsx'
+import Countries from './components/Countries.jsx'
 import GetRecipe from './functions/GetRecipe.jsx'
+import GetCountryList from './functions/GetCountriesList.jsx';
+
+
+/*const GetCurrentCountry = () => 
+  {
+    let CountryList = document.querySelector(".country-list");
+    let CountryChosen = CountryList.selectedOptions;
+    return CountryChosen[0].label;
+  }
+*/
 
 function App() {
+  const countriesData = GetCountryList();
   const recipesData = GetRecipe();
-
-  let RecipesData = recipesData.map((value => 
-    {
-      return (
-      <Recipes 
-        key = {value.idMeal}
-        img = {value.strMealThumb}
-        title={value.strMeal}
-        />
-      )
-    }))
-
+  
   return (
-
     <>
       <div className="App mx-auto">
-        <div className="xl:w-4/5 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-5 content-center mx-auto">
-            {RecipesData}
-        </div>
-      
+
+        <section className="text-center">
+            <select name="countries" className="country-list">
+              {countriesData.map((country => {
+                  return(
+                    <Countries
+                      key = {country.strArea} 
+                      Country = {country.strArea}
+                    />
+                  )}
+                  )
+                )
+              }
+            </select>
+
+            <div className="xl:w-4/5 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-5 content-center mx-auto">
+            {recipesData.map((recipe => 
+              {
+               return (
+                  <Recipes 
+                      key = {recipe.idMeal}
+                      img = {recipe.strMealThumb}
+                      title = {recipe.strMeal}
+                  />
+                )
+              }
+            ))}
+            </div>
+        </section>
+
       </div>
     </>
   )
 }
 
-export default App
+export default App;
