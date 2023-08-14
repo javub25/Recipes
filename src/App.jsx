512d00a354f52a4390f3afb3168556682b6185e2
Javi <javuberay@outlook.com>
2023-08-14 18:15:10 +0200

    Creating multiples sites with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -3,63 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import React from "react";
 import './index.css'
-import Recipes from './components/Recipes.jsx'
-import Countries from './components/Countries.jsx'
-import GetRecipe from './functions/GetRecipe.jsx'
-import GetCountryList from './functions/GetCountriesList.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from "./components/HomePage.jsx";
+import RecipeDetails from "./components/RecipeDetails.jsx";
 
+/*At this point, our app will have an initial component where the recipes will be displayed based on the user's choice of country, 
+all this will be inside the HomePage component.*/
+
+/*The second component called RecipeDetails will display the information of the recipe chosen by the user.*/
 function App() {
-  const countriesData = GetCountryList();
-
-  // ### Code to obtain the area of the country chosen by the user to display the recipes.
-  const [selectedCountry, setSelectedCountry] = React.useState("");
-
-  //Event that will save each value of select
-  const getCountry = (event) => {
-    setSelectedCountry(event.target.value);
-  };
-  // Once the country area has been chosen, the recipes are displayed.
-  const recipesData = GetRecipe(selectedCountry);
-  
   return (
     <>
       <div className="App mx-auto">
-
-        <section className="text-center">
-            <select name="countries" className="country-list" onClick={getCountry}>
-              <option value=""> -- Choose your area -- </option>
-              {countriesData.map((country => {
-                  return(
-                    <Countries
-                      key = {country.strArea} 
-                      Country = {country.strArea}
-                    />
-                  )}
-                  )
-                )
-              }
-            </select>
-            {
-              recipesData!==null &&
-
-              <div className="xl:w-4/5 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-5 content-center mx-auto">
-            {recipesData.map((recipe => 
-              {
-               return (
-                  <Recipes 
-                      key = {recipe.idMeal}
-                      img = {recipe.strMealThumb}
-                      title = {recipe.strMeal}
-                  />
-                )
-              }
-            ))}
-            </div>
-            }
-
-            
-        </section>
-
+        {/*Component HomePage will render Country NavBar and receips regarding country chosen by user*/}
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/recipe-details" element={<RecipeDetails/>}/>         
+          </Routes>
+        </Router>
       </div>
     </>
   )
