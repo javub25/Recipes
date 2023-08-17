8f359b7a93084b0815aa53e6534fd94f8a9e6298
Javi <javuberay@outlook.com>
2023-08-17 19:46:42 +0200

    Adding measurements

--- src/functions/GetIngredients.jsx
+++ src/functions/GetIngredients.jsx
@@ -1,5 +1,3 @@
-
-
 /*We will obtain from the 20 total ingredients, the ones that are neither null nor undefined.*/
 const GetIngredients = (Ingredients, MealsResponse) => 
 {
@@ -9,17 +7,21 @@ const GetIngredients = (Ingredients, MealsResponse) =>
     let posFirstIngredient = Object.keys(MealsResponse).indexOf("strIngredient1");
     let posLastIngredient = Object.keys(MealsResponse).indexOf("strIngredient20")
     
-
     let ingredient;
     let i = posFirstIngredient;
+    let measures;
     do
     {
          //Access to each property from value to strIngredient1 to strIngredient20    
          ingredient = MealsResponse[`strIngredient${countIngredients}`];
 
+         //Access to each property from value to strMeasure1 to strMeasure20    
+         measures = MealsResponse[`strMeasure${countIngredients}`];
+
         //Only values that are text will be saved
          if(ingredient!==null && ingredient!==""){
-            Ingredients.push(ingredient);
+            //Add measurements before the ingredient with empty space added.
+            Ingredients.push(measures.concat(" ",ingredient));
         }
         i++;
         countIngredients++;
