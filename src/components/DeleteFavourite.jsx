c7947f86e71af2e4739707c129182435aae128d6
Javi <javuberay@outlook.com>
2023-08-27 19:53:26 +0200

    Adding delete favourite functionality

---
+++ src/components/DeleteFavourite.jsx
@@ -0,0 +1,10 @@
+
+const DeleteFavourite = (idRecipe, removedFavorites, setRemovedFavorites) =>
+{
+    //It will return an object whose key doesn't contain the same RecipeID to be deleted.
+    setRemovedFavorites(removedFavorites.filter(item => item.id !== idRecipe))
+       
+    localStorage.setItem("FavouriteList", JSON.stringify(removedFavorites))
+}
+
+export default DeleteFavourite;
