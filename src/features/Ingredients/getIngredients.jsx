3266268f49ad6b65d37240176f58f21e4a10f1ef
Javi <javuberay@outlook.com>
2023-08-31 21:19:48 +0200

    Adding new push IngredientsData

--- src/features/Ingredients/getIngredients.jsx
+++ src/features/Ingredients/getIngredients.jsx
@@ -1,5 +1,5 @@
 /*We will obtain from the 20 total ingredients, the ones that are neither null nor undefined.*/
-const getIngredients = (Ingredients, MealsResponse) => 
+const getIngredients = (IngredientsData, MealsResponse) => 
 {
     //As strIngredient starts with one, it will be increased to Ingredient 20
     let countIngredients = 1;
@@ -7,13 +7,12 @@ const getIngredients = (Ingredients, MealsResponse) =>
     let posFirstIngredient = Object.keys(MealsResponse).indexOf("strIngredient1");
     let posLastIngredient = Object.keys(MealsResponse).indexOf("strIngredient20")
     
-    let ingredient;
-    let i = posFirstIngredient;
-    let measures;
+    let i = posFirstIngredient, ingredient, measures;
+    
     do
     {
          //Access to each property from value to strIngredient1 to strIngredient20    
-         ingredient = MealsResponse[`strIngredient${countIngredients}`];
+          ingredient = MealsResponse[`strIngredient${countIngredients}`];
 
          //Access to each property from value to strMeasure1 to strMeasure20    
          measures = MealsResponse[`strMeasure${countIngredients}`];
@@ -21,7 +20,7 @@ const getIngredients = (Ingredients, MealsResponse) =>
         //Only values that are text will be saved
          if(ingredient!==null && ingredient!==""){
             //Add measurements before the ingredient with empty space added.
-            Ingredients.push(measures.concat(" ",ingredient));
+            IngredientsData.push({name: ingredient, measures: measures})
         }
         i++;
         countIngredients++;
