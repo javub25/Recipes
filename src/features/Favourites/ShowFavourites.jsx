127ac83a9caec813fcb30b318da7da8c63388ed2
Javi <javuberay@outlook.com>
2023-09-02 19:32:05 +0200

    Adding section tag and marginTop and padding

--- src/features/Favourites/ShowFavourites.jsx
+++ src/features/Favourites/ShowFavourites.jsx
@@ -18,50 +18,52 @@ const ShowFavourites = () =>
     
    
     return (
-        <>        
-        <h1 className="text-center">Favourite Recipes</h1>
-        { Favourite.length > 0 ? (
-            <>
-            <div className="w-full flex items-center justify-center flex-wrap">
-            {Favourite.map((favourite => 
-                    {
-                        return (
-                            <div className="favourites items-center justify-center p-8">
-                                <div className="pr-6 pr-Mob">
-                                    <img src={favourite.photo} className="w-28"/>
-                                </div>
-                                <div className="py-4">
-                                    <h2 className="text-center">{favourite.title}</h2>
-                                    <br/>
-                                    <div className="text-center">
-                                        <Link
-                                            to={{
-                                                pathname: '/recipe-details',
-                                                search: `?recipeId=${favourite.id}`
-                                            }}
-                                        className="px-4 py-2 text-white bg-orange-400">
-                                            Check Recipe
-                                    </Link>
-                                    </div>
-                                    <div className="text-center pt-4">
-                                    <FontAwesomeIcon icon={faCircleXmark} onMouseOver={AddAnimateSwing} onMouseOut={DeleteAnimateSwing} onClick={() => deleteFavourite(favourite.id, removedFavorites, setRemovedFavorites)} className="text-2xl"/>
-                                    </div>
-                                </div>
-                               
-                            </div>)
-                }))}
+        <>
+            <section className="mt-24 px-4">
+                <h1 className="text-center">Favourite Recipes</h1>
+                { Favourite.length > 0 ? (
+                    <>
+                    <div className="w-full flex items-center justify-center flex-wrap">
+                    {Favourite.map((favourite => 
+                            {
+                                return (
+                                    <div className="favourites items-center justify-center p-8">
+                                        <div className="pr-6 pr-Mob">
+                                            <img src={favourite.photo} className="w-28"/>
+                                        </div>
+                                        <div className="py-4">
+                                            <h2 className="text-center">{favourite.title}</h2>
+                                            <br/>
+                                            <div className="text-center">
+                                                <Link
+                                                    to={{
+                                                        pathname: '/recipe-details',
+                                                        search: `?recipeId=${favourite.id}`
+                                                    }}
+                                                className="px-4 py-2 text-white bg-orange-400">
+                                                    Check Recipe
+                                            </Link>
+                                            </div>
+                                            <div className="text-center pt-4">
+                                            <FontAwesomeIcon icon={faCircleXmark} onMouseOver={AddAnimateSwing} onMouseOut={DeleteAnimateSwing} onClick={() => deleteFavourite(favourite.id, removedFavorites, setRemovedFavorites)} className="text-2xl"/>
+                                            </div>
+                                        </div>
+                                    
+                                    </div>)
+                        }))}
 
 
 
 
-            </div>
+                    </div>
 
-                
-            </>
-            
-        ):(
-            <h2 className="text-center">There aren't exist any favourite recipe</h2>
-        )}
+                        
+                    </>
+                    
+                ):(
+                    <h2 className="text-center">There aren't exist any favourite recipe</h2>
+                )}
+            </section>        
         </>   
     )
 }
