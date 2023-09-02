2cde6ded2d08e7551d3c06cf20e321199a74b825
Javi <javuberay@outlook.com>
2023-09-02 19:42:12 +0200

    Adding component Header with NavBar

---
+++ src/features/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import NavBar from '@features/NavBar/NavBar.jsx'
+
+const Header = () => 
+{    
+    return(
+        <>
+            <header>
+                <NavBar />
+            </header>
+        </>
+    )
+}
+
+export default Header;
