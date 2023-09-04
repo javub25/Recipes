0d05a38a7a81a0b038286f56474ce6aa35f38f76
Javi <javuberay@outlook.com>
2023-09-04 14:52:41 +0200

    Add section var to replace margin desktop mobile

--- src/tests/ShowNavBar.jsx
+++ src/tests/ShowNavBar.jsx
@@ -5,6 +5,7 @@ const ShowNavBar = (ShowNav, setShowNav) =>
 
     const navList = document.querySelector(".nav-content");
     let time;
+    const section = document.querySelector("section");
 
 
     if(ShowNav === true) {
@@ -14,13 +15,14 @@ const ShowNavBar = (ShowNav, setShowNav) =>
         },1500)
         navList.classList.add("animate__fadeOutLeft");
         navList.classList.remove("animate__fadeInLeft");
+        section.classList.replace("mt-80", "mt-24")
     }
     else
     {
         clearTimeout(time);
-        navList.classList.replace("hidden", "block");
-        navList.classList.add("animate__fadeInLeft");
-        navList.classList.remove("animate__fadeOutLeft")
+        section.classList.replace("mt-24", "mt-80")
+        navList.classList.add("block", "animate__fadeInLeft");
+        navList.classList.remove("hidden", "animate__fadeOutLeft")
     }
     return ShowNav;
 }
