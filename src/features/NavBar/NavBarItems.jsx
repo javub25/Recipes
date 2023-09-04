5c64a1db0c7355e99497247c318cafb8ef1c7373
Javi <javuberay@outlook.com>
2023-09-04 14:50:46 +0200

    Add routerDom Links and add ul tag

--- src/features/NavBar/NavBarItems.jsx
+++ src/features/NavBar/NavBarItems.jsx
@@ -1,15 +1,34 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome} from '@fortawesome/free-solid-svg-icons';
 import React from "react";
+import {Link} from 'react-router-dom';
 
 const NavBarItems = () => 
 {
     return (
-        <>        
-            <FontAwesomeIcon icon={faHome} className="text-white text-2xl"/>
-                <h2 className="text-1xl text-white">CATEGORIES</h2>
-                <h2 className="text-1xl text-white">AREAS</h2>
-                <h2 className="text-1xl text-white">FAVOURITES</h2>
+        <>
+            <ul className="nav-content-list w-full justify-between items-center">
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faHome} className="text-white text-2xl"/>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/categories">
+                        <h2 className="text-1xl text-white">CATEGORIES</h2>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/areas">
+                        <h2 className="text-1xl text-white">AREAS</h2>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/favourites">
+                        <h2 className="text-1xl text-white">FAVOURITES</h2>   
+                    </Link>
+                </li>
+            </ul>
         </>
     )
 }
