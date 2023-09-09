1b58454a726438a86e0509c64fc7105ef85d836f
Javi <javuberay@outlook.com>
2023-09-09 20:55:18 +0200

    Remove useffect hook

--- src/api/getRecipeInfo.jsx
+++ src/api/getRecipeInfo.jsx
@@ -1,13 +1,12 @@
 
-import axios from './axios';
 import React from "react";
-import getIngredients from '@features/Ingredients/getIngredients.jsx';
+import getUseffect from '@api/getUseffect.jsx';
 
 /*We're gonna get img, title, tags and instructions besides to idRecipe regarding Recipe chosen by user*/
 const getRecipeInfo = (idRecipe) => 
 {
     //Object will store that data for each recipe
-    const [RecipeInfo, setInfo] = React.useState({
+    const [RecipeInfo, setRecipeInfo] = React.useState({
             Category: "",
             Area: "",
             Ingredients: new Array(),
@@ -17,43 +16,8 @@ const getRecipeInfo = (idRecipe) =>
             StepByStep:"",
     });
 
-    React.useEffect(() => 
-    {  
-        const fetchData = async () => 
-        {
-            //Our response will return the above mentioned data of the chosen recipe.
-            const response = await axios.get(`/lookup.php?i=${idRecipe}`);
-
-            try
-            {
-                if(response.status === 200)
-                {
-                    const MealsResponse = response.data.meals[0];
-                    //Save the main ingredients of the selected receipe
-                    const IngredientsData = []
-
-                    getIngredients(IngredientsData, MealsResponse);
-
-                    
-                    setInfo({
-                        Category: MealsResponse.strCategory,
-                        Area: MealsResponse.strArea,
-                        Ingredients: IngredientsData, 
-                        Img: MealsResponse.strMealThumb,
-                        Title: MealsResponse.strMeal,
-                        Tags: MealsResponse.strTags,
-                        StepByStep: MealsResponse.strInstructions,
-                    })
-                }
-            }
-            catch(error){
-                throw new Error(error.message);
-            }
-        };    
-        fetchData();
-                
-    },[idRecipe])
-
+    getUseffect(`/lookup.php?i=${idRecipe}`, setRecipeInfo, "meals", idRecipe, "recipeInfo");
+    
     return RecipeInfo;
 }
 export default getRecipeInfo;
