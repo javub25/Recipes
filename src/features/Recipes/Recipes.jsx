05ac575437d251bf743cefc438004908e2b0de2d
Javi <javuberay@outlook.com>
2023-09-09 21:15:04 +0200

    adding conditional rendering for categories

--- src/features/Recipes/Recipes.jsx
+++ src/features/Recipes/Recipes.jsx
@@ -10,20 +10,34 @@ const Recipes = (props) =>
                 <img src={props.img} className="w-full rounded-lg"/>                
             </div>
             {/*With Link object we'll tell it that we want to pass each IdRecipe by Url when the user clicks More Details*/}
-            <div className="pb-8">
+            <div className="pb-8 text-center">
                 {/*to --> where Link has to go to send IdRecipe by Url
                    search --> the action that will make idRecipe to be sent by the url. 
                    To obtain the value we will access to recipeId
                 */}
-
-                <Link
-                    to={{
-                        pathname: '/recipe-details',
-                        search: `?recipeId=${props.idrecipe}`
-                    }}
-                    className="px-4 py-2 text-white bg-orange-400">
-                        More Details
-                </Link>
+                {props.view === "categories" ? (
+                    <>
+                    <h2 className="text-center text-black">{props.title}</h2>
+                    <br/>
+                        <Link
+                            to={{
+                                pathname: '/recipe-categories',
+                                search: `?categoryName=${props.title}`
+                            }}
+                            className="px-4 py-2 text-white bg-green-500 ">
+                                Recipes
+                        </Link>
+                    </>
+                ):
+                    <Link
+                        to={{
+                            pathname: '/recipe-details',
+                            search: `?recipeId=${props.idrecipe}`
+                        }}
+                        className="px-4 py-2 text-white bg-orange-400">
+                            More Details
+                    </Link>
+                }
             </div>
 
         </div>
