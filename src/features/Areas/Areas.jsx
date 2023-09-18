01eb84db4b58bbd5f1d658570ab0dba8f5308a45
javub25 <ju231415sad@gmail.com>
2023-09-18 13:42:07 +0200

    Fix page state variable issue

--- src/features/Areas/Areas.jsx
+++ src/features/Areas/Areas.jsx
@@ -19,16 +19,22 @@ const Areas = () =>
     const [page, setpage] = React.useState(0);
     const lengthRecipes = recipesData.Pages.length - 1;
 
-    console.log(recipesData.Pages)
+    /* The `React.useEffect` hook is used to perform side effects in a functional component. In this
+    case, it is used to reset the `page` state variable to 0 whenever the `selectedCountry` state
+    variable changes. */
+    React.useEffect(() => 
+    {
+      setpage(0);
+    },[selectedCountry])
+
     return (
         <>
-        <section className="text-center mt-40 mobile:mt-20 px-4">
+        <section className="text-center mt-40 mobile:mt-20 px-4 mb-40">
             <h1 className="text-2xl">Which area do you want to find out the recipes?</h1>
             <select name="countries" className="text-md w-52 country-list shadow-xl py-4 px-2 mt-12 border-solid border-r-8 border-transparent" onClick={getCountry}> 
             <option value="" className="text-left">Choose option</option>
-
-              {countriesData.map((country => <option key = {country.strArea} value={country.strArea} className="text-left">{country.strArea}</option>)
-              )
+              {
+              countriesData.map((country => <option key = {country.strArea} value={country.strArea} className="text-left">{country.strArea}</option>))
             }
           </select>
         {
@@ -36,7 +42,7 @@ const Areas = () =>
           <>
             {
             recipesData.Total > 0 &&
-
+            
               <>
                 <div className="py-16">
                   <h3>Great! it seems we found <b>{recipesData.Total} {recipesData.Total === 1 ? "recipe" : "recipes"}</b>.</h3>
