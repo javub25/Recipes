51fcaef5c019b4382a574489a317e2fcdb19f87c
javub25 <ju231415sad@gmail.com>
2023-09-20 17:46:04 +0200

    Adding marginBottom

--- src/features/Categories/Categories.jsx
+++ src/features/Categories/Categories.jsx
@@ -6,9 +6,10 @@ const Categories = () =>
 {
     const CategoryList = getCategories();
 
+
     return (
         <>
-        <section className="text-center mt-40 mobile:mt-20 px-4">
+          <section className="text-center mt-40 mobile:mt-20 px-4 mb-40">
             <h1 className="text-2xl pb-8">Categories</h1>
             {
                 <div className="xl:w-4/5 grid sm:grid-cols-3 xl:grid-cols-5 gap-x-1 gap-y-8 content-center mx-auto pb-8">
