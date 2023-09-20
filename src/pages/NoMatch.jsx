2f3d206655a8c01751c56c3912992bcba1d48349
javub25 <ju231415sad@gmail.com>
2023-09-20 16:10:47 +0200

    Adding page not found

---
+++ src/pages/NoMatch.jsx
@@ -0,0 +1,9 @@
+const NoMatch = () => 
+{
+    return (
+        <section className="text-center mt-40 mobile:mt-20 px-4">
+            <h1 className="text-2xl pb-8">Oopss this website doesn't exist!</h1>
+        </section>
+    )
+}
+export default NoMatch;
